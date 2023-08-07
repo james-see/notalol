261bdb8459eb1254fb296266c9ffcc1bf4b8f96a
James Campbell <jcampbell@finchcomputing.com>
2023-08-07 10:50:30 -0400

    added local css bulma and promo images

--- backup/contextScript.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.sendMessage({ action: "createContextMenu" });
-
-chrome.runtime.onMessage.addListener(function (message) {
-  if (message.action === "sendToPopup") {
-    const selectedText = message.text;
-    chrome.runtime.sendMessage({ action: "sendToPopup", text: selectedText });
-  }
-});
